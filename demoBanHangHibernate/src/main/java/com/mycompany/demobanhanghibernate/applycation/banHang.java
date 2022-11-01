2c6ff1ede44a9296d2a83466030c03c5259e512c
Ngô Vũ Ngọc Hào <ngovungochao14112003@gmail.com>
2022-11-01 22:42:10 +0700

    first commit

---
+++ demoBanHangHibernate/src/main/java/com/mycompany/demobanhanghibernate/applycation/banHang.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.demobanhanghibernate.applycation;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class banHang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OKOkokokk Nhóm 4");
+    }
+    
+}
