2e17cbb476a4e7a3ee79525ed4b68a95696783fa
Ngô Vũ Ngọc Hào <ngovungochao14112003@gmail.com>
2022-11-03 20:47:34 +0700

    Update banHang.java

--- demoBanHangHibernate/src/main/java/applycation/banHang.java
+++ demoBanHangHibernate/src/main/java/applycation/banHang.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mycompany.demobanhanghibernate.applycation;
+package applycation;
 
 /**
  *
@@ -14,9 +14,7 @@ public class banHang {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //update oke
-       //buon
-       //update  thanh cong
+        //code new
     }
-    
+
 }
